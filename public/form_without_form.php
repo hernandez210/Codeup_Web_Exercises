8ee2278a1bc89b3761f5afd22f0a34905ff92d23
Hector Hernandez <hernandez210@me.com>
2015-04-16 18:53:17 -0500

    Second Test

---
+++ public/form_without_form.php
@@ -0,0 +1,36 @@
+<?php
+	var_dump($_GET);
+	?>
+<DOCTYPE html>
+<html>
+<head>
+	<title>Form without forms</title>
+</head>
+<body>
+<p><h1>Forms_without_forms</h1>
+</p>
+<a href="?greeting=Hello+codeup!">click me!</a>
+<p>
+
+	<form method="GET" action="/my_first_form.php">
+	
+	<label for="username">Username: </label><input type="text" name="username" placeholder="username" id="username">
+	<label>
+		password:
+		<input type="password" name="password"
+	<input type="password" name="password" placeholder="password">
+	</p>
+	<p>
+	<textarea name="comment"> this is my comment</textarea> <br>
+	<label> is Codeup awesome? 
+	<input type="checkbox" value="of course" name="codeup_awesome" checked="checked">
+	</label>
+	
+	<p>do you like these foods:</p>
+	<label>meat <input type="checkbox" name="meat" value="yes"</label>
+	<label>yogurt <input type="checkbox" name="yogurt" value="yes"</label>
+
+	<p><input type="submit"></p>
+</p>
+	
+</form>
---
+++ public/try_is_again.php
@@ -0,0 +1,21 @@
+<?php
+ var_dump($_POST):
+ ?>
+ <!DOCTYPE html>
+<head>
+	<title>Let me try this again</title>
+</head>
+<body>
+<h1>Try this again</h1>
+<p> This webpage is a second try at everything i have learned </p>
+
+
+
+
+</body>
+
+
+
+
+
+ </html>
