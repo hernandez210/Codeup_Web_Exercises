f9949ee0fe3778a862b6bb33fe19075f7ba1589b
Hector Hernandez <hernandez210@me.com>
2015-04-25 15:43:54 -0500

    sat

---
+++ public/js/for_loops.js
@@ -0,0 +1,11 @@
+"use strict";
+for (var i = 100; i > 4; i -= 5) {
+    console.log('for loop iteration #' + i);
+}
+
+// var i = 2;
+
+// while (i < 10) {
+//     console.log('while loop iteration #' * i);
+//     i++;
+// }
