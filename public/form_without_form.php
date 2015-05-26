0e21056fa29df81905db31c0ef8632bb016641dd
Hector Hernandez <hernandez210@me.com>
2015-05-26 16:48:13 -0500

    I have gone back and modified each file to make my code easier to read. I am going through each file and this commit only contains the part up to the file the is japan_flag.html.....Will continue to correct the remaining files in the following days.

--- public/form_without_form.php
+++ public/form_without_form.php
@@ -2,36 +2,46 @@
 	var_dump($_GET);
 ?>
 <!DOCTYPE HTML>
-	<html>
-		<head>
-			<title>Form without forms</title>
-		</head>
-		<body>
-			<p><h1>
+<html>
+	<head>
+		<title>Form without forms</title>
+	</head>
+	<body>
+		<p>
+			<h1>
 				Forms_without_forms
-			</h1></p>
-				<a href="?greeting=Hello+codeup!">click me!</a>
-			<p>
-				<form method="GET" action="/my_first_form.php">
+			</h1>
+		</p>
+		
+		<a href="?greeting=Hello+codeup!">Click me!</a>
+		
+		<p>
+			<form method="GET" action="/my_first_form.php">
 				<label for="username">Username: </label>
 				<input type="text" name="username" placeholder="username" id="username">
 				<label for="password">Password:</label>
 				<input type="password" name="password" placeholder="password">
-			</p>
-			<p>
-				<textarea name="comment"> this is my comment</textarea> <br>
-				<label> is Codeup awesome? 
+		</p>
+		<p>
+				<textarea name="comment">
+					This is my comment
+				</textarea>
+				<br>
+				
+				<label> Is Codeup awesome? 
 				<input type="checkbox" value="of course" name="codeup_awesome" checked="checked">
 				</label>
-			<p>
-				do you like these foods:
-			</p>
-				<label>meat <input type="checkbox" name="meat" value="yes"</label>
-				<label>yogurt <input type="checkbox" name="yogurt" value="yes"</label>
-			<p>
-				<input type="submit">
-			</p>
-			</p>
-	
-				</form>
-	</html>
+
+				<p>
+					Do you like these foods:
+				</p>
+					<label>Meat <input type="checkbox" name="meat" value="yes"</label>
+					<label>Yogurt <input type="checkbox" name="yogurt" value="yes"</label>
+				<p>
+					<input type="submit">
+				</p>
+			
+			</form>
+		</p>
+	</body>
+</html>
